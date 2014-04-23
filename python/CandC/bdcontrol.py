97f30c1c50aa676c8722b8c87aa62d4812194c22
Tek Tengu <tektengu@teksecgrp.com>
2014-04-23 13:28:17 -0400

    Initial add of CandC module and bdcontrol.py class (core CandC data and behaviors

---
+++ python/CandC/bdcontrol.py
@@ -0,0 +1,17 @@
+__author__ = 'tek tengu'
+
+class CandC:
+    """The command and control class. Defines schedules of contact (primary and secondary); contact protocols:"""
+    def __init__(self):
+        return
+
+
+class Schedule:
+    """This is the class that defines any schedule related data and behaviors"""
+    def __init__(self):
+        return
+
+class Cprotocol:
+    """This is the class that defines any contact protocol related data and behaviors"""
+    def __init__(self):
+        return
